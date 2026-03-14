85f9065ae549736353dd99c8d0b02d7d2bd57737
WBO <waelbenothmane.qa@gmail.com>
2026-03-14 14:30:53 +0100

    add sauce option

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,6 +1,5 @@
-import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -9,30 +8,33 @@ import java.net.URL;
 
 public class GoogleTest {
 
-
     @Test
     @Parameters({"browser"})
-    public static void openGoogle(String browser) throws Exception {
+    public void openGoogle(String browser) throws Exception {
 
         String username = System.getenv("SAUCE_USERNAME");
         String accessKey = System.getenv("SAUCE_ACCESS_KEY");
 
-        MutableCapabilities caps = new MutableCapabilities();
-        caps.setCapability("browserName", browser);
-        caps.setCapability("browserVersion", "latest");
-        caps.setCapability("platformName", "Windows 11");
-        caps.setCapability("name", "Google Test");
-        caps.setCapability("build", "TeamCity Build");
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("build", "TeamCity Build");
+        sauceOptions.setCapability("name", "Google Test");
+
+        MutableCapabilities capabilities = new MutableCapabilities();
+        capabilities.setCapability("browserName", browser);
+        capabilities.setCapability("browserVersion", "latest");
+        capabilities.setCapability("platformName", "Windows 10");
+
+        capabilities.setCapability("sauce:options", sauceOptions);
 
         WebDriver driver = new RemoteWebDriver(
                 new URL("https://" + username + ":" + accessKey +
                         "@ondemand.eu-central-1.saucelabs.com/wd/hub"),
-                caps);
+                capabilities
+        );
 
         driver.get("https://www.google.com");
-        driver.manage().window().maximize();
-        Thread.sleep(5000);
-        System.out.println("Title of the page is: " + driver.getTitle());
+
+        System.out.println(driver.getTitle());
 
         driver.quit();
 
