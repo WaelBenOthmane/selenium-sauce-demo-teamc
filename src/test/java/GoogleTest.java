378a155d6378a949947412dd95c1727d52dd66c4
WBO <waelbenothmane.qa@gmail.com>
2026-03-14 11:43:45 +0100

    add parallele test

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,6 +1,8 @@
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.net.URL;
@@ -9,26 +11,26 @@ public class GoogleTest {
 
 
     @Test
-    public static void openGoogle() throws Exception {
+    @Parameters({"browser"})
+    public static void openGoogle(String browser) throws Exception {
 
         String username = System.getenv("SAUCE_USERNAME");
         String accessKey = System.getenv("SAUCE_ACCESS_KEY");
 
-        ChromeOptions options = new ChromeOptions();
-        options.setCapability("browserVersion", "latest");
-        options.setCapability("platformName", "Windows 11");
+        MutableCapabilities caps = new MutableCapabilities();
+        caps.setCapability("browserName", browser);
+        caps.setCapability("browserVersion", "lastest");
+        caps.setCapability("platformName", "Windows 11");
 
         WebDriver driver = new RemoteWebDriver(
                 new URL("https://" + username + ":" + accessKey +
                         "@ondemand.eu-central-1.saucelabs.com/wd/hub"),
-                options);
+                caps);
 
         driver.get("https://www.google.com");
         driver.manage().window().maximize();
         Thread.sleep(5000);
         System.out.println("Title of the page is: " + driver.getTitle());
-        System.out.println("Test SauceLabs");
-        System.out.println("Test Job Overview");
 
         driver.quit();
 
